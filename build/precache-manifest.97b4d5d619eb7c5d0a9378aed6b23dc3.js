8faaaeb8c8a1c82bd7adef5f319fca291729e483
gurulnd <guruprasadd@outlook.com>
2019-03-01 00:18:19 +0530

    first commit

---
+++ build/precache-manifest.97b4d5d619eb7c5d0a9378aed6b23dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1339cd3fdc875602eec7",
+    "url": "/static/js/main.1339cd3f.chunk.js"
+  },
+  {
+    "revision": "7a4ce7c974838f6c3728",
+    "url": "/static/js/1.7a4ce7c9.chunk.js"
+  },
+  {
+    "revision": "1339cd3fdc875602eec7",
+    "url": "/static/css/main.18451c9e.chunk.css"
+  },
+  {
+    "revision": "9fa4170a088b4ae9d300e2322bca29ca",
+    "url": "/index.html"
+  }
+];
